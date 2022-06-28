3acf01310807246578f369d2dd397e591cdc0da4
applesthepi <applesthepi@gmail.com>
2022-06-28 18:45:04 -0400

    fpng

---
+++ atpnn/include/ann/visualizer.cpp
@@ -0,0 +1,9 @@
+#include "visualizer.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+void ann::visualize_nn(ann::nn_structure* structure, const std::string& file_name)
+{
+
+}
---
+++ atpnn/include/ann/visualizer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "components.hpp"
+
+#include <string>
+
+namespace ann
+{
+void visualize_nn(ann::nn_structure* structure, const std::string& file_name);
+}
